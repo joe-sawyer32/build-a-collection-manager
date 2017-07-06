42de4bbf7952e8d87caf119bd7c9e584bdb99b39
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-06 14:02:58 -0500

    Album schema created and exported

--- models/Album.js
+++ models/Album.js
@@ -1,2 +1,48 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-module.exports = ;
+var albumSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  releaseDate: {
+    type: Number,
+    required: true
+  },
+  label: String,
+  genre: {
+    type: String,
+    enum: [
+      "rock",
+      "pop",
+      "country",
+      "rap",
+      "hip-hop",
+      "r&b",
+      "classical",
+      "metal",
+      "alternative",
+      "electronic",
+      "jazz",
+      "blues",
+      "instrumental",
+      "spoken word",
+      "unknown"
+    ],
+    default: "unknown"
+  },
+  tracks: [
+    {
+      title: String,
+      length: Number,
+      single: Boolean
+    }
+  ]
+});
+
+module.exports = mongoose.model("Album", albumSchema);
